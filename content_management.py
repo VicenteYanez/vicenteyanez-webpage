b280170bdac71b6c6442bbcd1b9e9eb25aeb0704
Vicente Yáñez <vicenteyanez@protonmail.com>
2017-08-06 18:31:47 -0400

    added 404 and DB connection

---
+++ content_management.py
@@ -0,0 +1,31 @@
+
+from datetime import date
+
+
+class Content():
+    content_dict = {"Data Analysis":
+                    [["Heath and Earthquakes", "heath-earthquakes"],
+                     ["Stress on Volcanos: Villarica", "stress-villarica"]],
+                    "Geology":
+                    [["The History of Concepcion", "concepcion-history"],
+                     ["The History of Toronto", "toronto-history"]],
+                    "Education":
+                    [["How to make boring thing fun", "boring-things-fun"]]}
+
+    def top5():
+        """
+        Method that calculates the last 5 post
+        """
+        d1 = date(2005, 7, 14)
+        d2 = date(2005, 7, 15)
+        d3 = date(2005, 7, 16)
+        d4 = date(2005, 7, 17)
+        d5 = date(2005, 7, 18)
+
+        post_list = [["Heath and Earthquakes", "heath-earthquakes", d1],
+                     ["Stress on Volcanos: Villarica", "stress-villarica", d2],
+                     ["The History of Concepcion", "concepcion-history", d3],
+                     ["The History of Toronto", "toronto-history", d4],
+                     ["How to make boring thing fun", "boring-things-fun", d5]]
+
+        return post_list
--- flask_app.py
+++ flask_app.py
@@ -2,13 +2,40 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from content_management import Content
 
 app = Flask(__name__)
+
+# DB parameters
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="vicenteyanez",
+    password="7candadospara7tablas",
+    hostname="vicenteyanez.mysql.pythonanywhere-services.com",
+    databasename="vicenteyanez$personal-website-test")
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# db connection
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
+
+@app.route('/blog/')
+def content():
+    return render_template("content.html", menu=Content.content_dict,
+                           top5=Content.top5())
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", menu=Content.content_dict,
+                           top5=Content.top5())
 
 
 if __name__ == "__main__":
