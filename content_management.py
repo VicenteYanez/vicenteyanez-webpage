820ab6e13bcf04e94549f7ff14f13b3a1a81d065
Vicente Yáñez <vicenteyanez@protonmail.com>
2017-09-01 15:25:03 -0400

    webpage ready!

--- content_management.py
+++ content_management.py
@@ -1,34 +1,33 @@
 
 from datetime import date
+from operator import itemgetter
 
 
 class Content():
-    content_dict = {"Data Analysis":
-                    [["Heath and Earthquakes", "weather-earthquakes"],
-                     ["Stress on Volcanos: Villarica", "stress-villarica"]],
-                    "Geology":
-                    [["The History of Concepcion", "concepcion-history"],
-                     ["The History of Toronto", "toronto-history"]],
-                    "Education":
-                    [["How to make boring thing fun", "boring-things-fun"]]}
+    def __init__(self):
+        self.content_dict = {"Data Analysis":
+                             [["Weather and Earthquakes",
+                               "weather-earthquakes",
+                               date(2017, 8, 31)]],
+                             "Education": [],
+                             "Geology": []}
 
-    projects_list = [["GFA: GNSS Field Analysis",
-                      "https://github.com/VicenteYanez/GFA"]]
+        self.projects_list = [["GFA: GNSS Field Analysis",
+                               "https://github.com/VicenteYanez/GFA"]]
 
-    def top5():
+    def top5(self):
         """
         Method that calculates the last 5 post
         """
-        d1 = date(2005, 7, 14)
-        d2 = date(2005, 7, 15)
-        d3 = date(2005, 7, 16)
-        d4 = date(2005, 7, 17)
-        d5 = date(2005, 7, 18)
+        tagdata = self.content_dict['Data Analysis']
+        tagedu = self.content_dict['Education']
+        # add any other tag in the dictionaty !!!!!!!!!!!!!!!!!!!!
 
-        post_list = [["Heath and Earthquakes", "heath-earthquakes", d1],
-                     ["Stress on Volcanos: Villarica", "stress-villarica", d2],
-                     ["The History of Concepcion", "concepcion-history", d3],
-                     ["The History of Toronto", "toronto-history", d4],
-                     ["How to make boring thing fun", "boring-things-fun", d5]]
+        allcontent = tagdata + tagedu
 
-        return post_list
+        sortedcontent = sorted(allcontent, key=itemgetter(2), reverse=True)
+
+        return sortedcontent[:5]
+
+    def load_dates(self):
+        return
--- flask_app.py
+++ flask_app.py
@@ -2,23 +2,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, Markup
-from flask_sqlalchemy import SQLAlchemy
 
 from content_management import Content
 
 app = Flask(__name__)
 
-# DB parameters
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="root",
-    password="7candadospara7tablas",
-    hostname="nasus",
-    databasename="personal_website_test")
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# db connection
-db = SQLAlchemy(app)
+listcontent = Content()
 
 
 @app.route('/')
@@ -26,42 +15,45 @@ def homepage():
     return render_template("index.html")
 
 
-@app.route('/projects')
+@app.route('/projects/')
 def projects():
-    return render_template("projects.html", projects=Content.projects_list)
+    return render_template("projects.html", projects=listcontent.projects_list)
 
 
 @app.route('/blog/')
-def blog_content(content=None):
-    return render_template("content.html", menu=Content.content_dict,
-                           top5=Content.top5(), content=content)
+@app.route('/blog/categories/<category>')
+def blog_content(content=None, category='None'):
+    return render_template("content.html", menu=listcontent.content_dict,
+                           top5=listcontent.top5(), content=content,
+                           category=category)
 
 
-@app.route('/aboutme')
+@app.route('/aboutme/')
 def aboutme():
     return render_template("aboutme.html")
 
 
-@app.route('/data')
+@app.route('/data/')
 def data():
     return render_template("data.html")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", menu=listcontent.content_dict,
+                           top5=listcontent.top5())
+
+
 # blog posts
 @app.route('/blog/weather-earthquakes/')
-def weather_earthquakes():
+def weather_earthquakes(category='empty'):
     template = "content/weather-earthquakes/weather-earthquakes.html"
     content_file = open(template, 'r')
     content = Markup(content_file.read())
     content_file.close()
-    return render_template("content.html", menu=Content.content_dict,
-                           top5=Content.top5(), content=content)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html", menu=Content.content_dict,
-                           top5=Content.top5())
+    return render_template("content.html", menu=listcontent.content_dict,
+                           top5=listcontent.top5(), content=content,
+                           category=category)
 
 
 if __name__ == "__main__":
